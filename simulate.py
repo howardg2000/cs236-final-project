Refactor and fix expected value calculation

simulate.py
@@ -1,30 +1,31 @@
 import numpy as np
 from mdp import MDP
+from planner import GreedyPlanner
 
 
-class Planner:
-    def __init__(self, num_actions, action_means, transition_probs) -> None:
-        self.num_actions = num_actions
-        self.action_means = np.array(action_means)
-        self.transition_probs = np.array(transition_probs)
+class Simulation:
+    def __init__(self, num_actions, reward_dists, reward_params, transition_probs, planner) -> None:
+        self.env = MDP(num_actions, reward_dists,
+                       reward_params, transition_probs)
+        self.reward_means = [(param[0] + param[1]) /
+                             2 for param in reward_params]
+        self.planner = planner(
+            num_actions, self.reward_means, transition_probs)
 
-        self.last_explored = np.array([-np.inf] * num_actions)
-        self.rewards = np.array([-np.inf] * num_actions)
+    def simulate(self, n_iter, verbose=False):
+        total_reward = 0.0
+        optimal_reward = 0.0
+        for t in range(n_iter):
+            action = self.planner.get_action(t)
+            reward, all_rewards = self.env.get_reward(action, verbose=True)
+            if verbose:
+                print(all_rewards, action)
+            self.planner.record_reward(action, t, reward)
 
-    def get_action(self, time_step):
-        """Get next action to explore."""
-        if time_step < self.num_actions:
-            return time_step
-        time_since_explored = time_step - self.last_explored
-        prob_of_reward_change = transition_probs ** time_since_explored
-        expected_rewards = prob_of_reward_change * self.action_means + \
-            (1 - prob_of_reward_change) * self.rewards
-        return np.argmax(expected_rewards)
+            total_reward += reward
+            optimal_reward += max(all_rewards)
 
-    def record_reward(self, action, timestep, reward):
-        """Record reward for action."""
-        self.last_explored[action] = timestep
-        self.rewards[action] = reward
+        print(total_reward, optimal_reward)
 
 
 if __name__ == "__main__":
@@ -34,21 +35,9 @@ if __name__ == "__main__":
     reward_dists = [np.random.uniform, np.random.uniform]
     reward_params = [(-1, 5), (-5, 5)]
     transition_probs = [0.1, 0.1]
-    env = MDP(num_actions, reward_dists, reward_params, transition_probs)
+    planner = GreedyPlanner
 
-    reward_means = [(param[0] + param[1]) / 2 for param in reward_params]
-    planner = Planner(num_actions, reward_means, transition_probs)
+    sim = Simulation(num_actions, reward_dists,
+                     reward_params, transition_probs, planner)
 
-    n_iter = 25
-    total_reward = 0.0
-    optimal_reward = 0.0
-    for t in range(n_iter):
-        action = planner.get_action(t)
-        reward, all_rewards = env.get_reward(action, verbose=True)
-        # print(all_rewards, action)
-        planner.record_reward(action, t, reward)
-
-        total_reward += reward
-        optimal_reward += max(all_rewards)
-
-    print(total_reward, optimal_reward)
+    sim.simulate(25, verbose=True)