6a2c6af9dff278fd8f6e22d9c849dc6956e4ded3
howardg2000 <hjguo@college.harvard.edu>
2022-12-06 16:54:09 -0500

    Add greedy planner

--- simulate.py
+++ simulate.py
@@ -1,13 +1,54 @@
 import numpy as np
 from mdp import MDP
 
+
+class Planner:
+    def __init__(self, num_actions, action_means, transition_probs) -> None:
+        self.num_actions = num_actions
+        self.action_means = np.array(action_means)
+        self.transition_probs = np.array(transition_probs)
+
+        self.last_explored = np.array([-np.inf] * num_actions)
+        self.rewards = np.array([-np.inf] * num_actions)
+
+    def get_action(self, time_step):
+        """Get next action to explore."""
+        if time_step < self.num_actions:
+            return time_step
+        time_since_explored = time_step - self.last_explored
+        prob_of_reward_change = transition_probs ** time_since_explored
+        expected_rewards = prob_of_reward_change * self.action_means + \
+            (1 - prob_of_reward_change) * self.rewards
+        return np.argmax(expected_rewards)
+
+    def record_reward(self, action, timestep, reward):
+        """Record reward for action."""
+        self.last_explored[action] = timestep
+        self.rewards[action] = reward
+
+
 if __name__ == "__main__":
-    np.random.seed(42)
+    np.random.seed(50)
 
-    env = MDP(2, [np.random.uniform, np.random.normal],
-              [(-1, 5), (-5, 5)], [0.1, 0.1])
+    num_actions = 2
+    reward_dists = [np.random.uniform, np.random.uniform]
+    reward_params = [(-1, 5), (-5, 5)]
+    transition_probs = [0.1, 0.1]
+    env = MDP(num_actions, reward_dists, reward_params, transition_probs)
 
-    n_iter = 10
-    for i in range(n_iter):
-        reward, all_rewards = env.get_reward(0, verbose=True)
-        print(all_rewards)
+    reward_means = [(param[0] + param[1]) / 2 for param in reward_params]
+    planner = Planner(num_actions, reward_means, transition_probs)
+
+    n_iter = 25
+    total_reward = 0.0
+    optimal_reward = 0.0
+    for t in range(n_iter):
+        action = planner.get_action(t)
+        reward, all_rewards = env.get_reward(action, verbose=True)
+        # print(all_rewards, action)
+        planner.record_reward(action, t, reward)
+
+        total_reward += reward
+        optimal_reward += max(all_rewards)
+
+    print(total_reward, optimal_reward)
